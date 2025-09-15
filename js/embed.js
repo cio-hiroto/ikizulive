ce43be205b497cbf2c56a622e33cb2d13a876a8e
ll_club <cioengjp@gmail.com>
2025-09-16 00:10:35 +0900

    ikizulive slider

--- js/embed.js
+++ js/embed.js
@@ -1,14 +1,91 @@
 $(function() {
     $.getJSON('json/data.json', function(data) {
         var $list = $('.twitter-embed-list');
-        $list.empty();
-        data.twitter_posts.forEach(function(post) {
-            var tweetUrl = post.url;
-            var embedHtml =
-                '<blockquote class="twitter-tweet"><a href="' + tweetUrl + '"></a></blockquote>';
-            var $item = $('<div class="twitter-embed-item"></div>').html(embedHtml);
-            $list.append($item);
+        var $items = $list.find('.twitter-embed-item');
+        var $dots = $('.twitter-carousel-dots');
+        var $prev = $('.twitter-carousel-prev');
+        var $next = $('.twitter-carousel-next');
+        $dots.empty();
+        var posts = data.twitter_posts;
+        $items.each(function(idx){
+            var post = posts[idx];
+            if(post){
+                var tweetUrl = post.url;
+                var embedHtml = '<blockquote class="twitter-tweet"><a href="' + tweetUrl + '"></a></blockquote>';
+                $(this).html(embedHtml);
+            }else{
+                $(this).html("");
+            }
+            var $dot = $('<span class="twitter-carousel-dot"></span>');
+            if(idx === 0) $dot.addClass('active');
+            $dots.append($dot);
         });
+        // 事前レンダリング用に全itemへ.preloadクラスを付与
+        $items.addClass('preload').show();
+        if(window.twttr && window.twttr.widgets){
+            twttr.widgets.load($list[0]);
+        }
+        // レンダリング後に1件だけ表示（preload解除）
+        setTimeout(function(){
+            $items.removeClass('preload');
+            showSlide(0);
+        }, 1200);
+        var current = 0;
+        var timer;
+        var slideCount = posts.length;
+        function showSlide(idx) {
+            $list.find('.twitter-embed-item').removeClass('center active').hide();
+            var count = $list.find('.twitter-embed-item').length;
+            // 中央のみ表示
+            $list.find('.twitter-embed-item').eq(idx).addClass('center active').show();
+            $dots.find('.twitter-carousel-dot').removeClass('active');
+            $dots.find('.twitter-carousel-dot').eq(idx).addClass('active');
+        }
+        function nextSlide() {
+            current = (current + 1) % slideCount;
+            showSlide(current);
+        }
+        function prevSlide() {
+            current = (current - 1 + slideCount) % slideCount;
+            showSlide(current);
+        }
+        function resetTimer() {
+            clearInterval(timer);
+            timer = setInterval(nextSlide, 6000);
+        }
+        $dots.on('click', '.twitter-carousel-dot', function() {
+            var idx = $dots.find('.twitter-carousel-dot').index(this);
+            current = idx;
+            showSlide(current);
+            resetTimer();
+        });
+        $next.on('click', function() {
+            nextSlide();
+            resetTimer();
+        });
+        $prev.on('click', function() {
+            prevSlide();
+            resetTimer();
+        });
+        // スワイプ対応
+        var startX = null;
+        $list.on('touchstart', function(e) {
+            startX = e.originalEvent.touches[0].clientX;
+        });
+        $list.on('touchend', function(e) {
+            if(startX === null) return;
+            var endX = e.originalEvent.changedTouches[0].clientX;
+            if(endX - startX > 50) {
+                prevSlide();
+                resetTimer();
+            } else if(startX - endX > 50) {
+                nextSlide();
+                resetTimer();
+            }
+            startX = null;
+        });
+        showSlide(current);
+        resetTimer();
         function loadTwitterWidgets() {
             if (window.twttr && window.twttr.widgets) {
                 window.twttr.widgets.load();
